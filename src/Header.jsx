0336f605a9724138b64378814804bafd977c8619
Diego Avila <dandida95@gmail.com>
2020-10-06 22:45:23 -0600

    Changed color and type of button in the Header Bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
-import {Menu} from '@material-ui/icons'
+import {AppBar, Toolbar, Typography, Button} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
 
 const headerStyle = makeStyles({
     header: {
         background: 'linear-gradient(45deg,#141E30  35%, #243B55 95%)'
+    },
+    button: {
+        color: '#FFF',
+        borderColor: '#7C0303',
+        borderWidth: '2px'
+
     }
 }) 
 
 function Header (){
     const style = headerStyle();
     return (<AppBar position="relative" className={style.header}>
-<Toolbar >
-  <IconButton edge="start" color="inherit">
-    <Menu />
-  </IconButton>
-  <Typography variant="h6">
-    SeeSound
-  </Typography>
-  <Button color="inherit"></Button>
-</Toolbar>
+    <Toolbar >
+        <Button  variant = 'outlined' className={style.button}>
+  <Typography variant="h6">SeeSound</Typography>
+        </Button>
+    </Toolbar>
 </AppBar>)}
 
 export default Header;
