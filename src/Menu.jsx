37d3748090a830de9945a662a10392535cdc549f
Diego Avila <dandida95@gmail.com>
2020-10-07 22:39:42 -0600

    fixed position of menu to bottom

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Menu, MenuItem, Button, Typography} from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles, withStyles } from '@material-ui/styles'
 
 
 const buttonStyle = makeStyles({
@@ -12,32 +12,64 @@ const buttonStyle = makeStyles({
     }
 })
 
+const StyledMenu = withStyles({
+        paper: {
+        border: '2px solid #7C0303',
+        background: 'linear-gradient(45deg,#141E30  35%, #243B55 95%)',
+        color: '#FFF'
+        },
+    })((props)=>(
+        <Menu
+            getContentAnchorEl={null}
+            anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+            }}
+            transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+            }
+            
+        }
+            {...props}
+      />
+));
+
 function MainMenu(){
     const [anchorEl, setAnchorEl] = React.useState(null)
     const style = buttonStyle();
-    const handleClick = (event) =>{
+    const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
-    }
-
-    const handleClose = () => {
+  
+      };
+    const handleClose = () =>{
         setAnchorEl(null);
     }
+
     return (<div>
 
+         
         <Button variant = 'outlined' className={style.button} onClick={handleClick}>
             <Typography variant= "h6"> SeeSound </Typography>
         </Button>
-        <Menu
-        id="simple-menu"
+    <StyledMenu
+        id="customized-menu"
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
+        <MenuItem>
+            Select Image
+        </MenuItem>
+        <MenuItem>
+         Upload Image
+        </MenuItem>
+        <MenuItem>
+         Some other option
+        </MenuItem>
+      </StyledMenu>
+
       </div>)
 }
 
