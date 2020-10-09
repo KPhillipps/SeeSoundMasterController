3cc4cc70b1e086ae426487ef64211a3af05227ba
Diego Avila <dandida95@gmail.com>
2020-10-09 00:42:45 -0600

    changed padding on the container component

--- src/Container.jsx
+++ src/Container.jsx
@@ -3,16 +3,27 @@ import {Container, Typography, makeStyles}  from '@material-ui/core';
 
 
 const style = makeStyles({
+  typography: {
+    backgroundColor: '#1d1d1d', 
+    height: '100vh', 
+    color: "#FFF",
+    borderLeftColor: '#7C0303',
+    borderRightColor: '#7C0303',
+    border: 'solid 2px',
+    borderBottomColor: '#1d1d1d',
+    borderTopColor: '#1d1d1d'
+  },
   container: {
-    backgroundColor: '#1d1d1d', height: '100vh', color: "#FFF"
+    background: '#7C0303'
   }
+
 })
 
 function AppContainer(){
     const containerStyle = style();
     return (
-        <Container maxWidth={false}  >
-          <Typography component="div" className={containerStyle.container}/>
+        <Container maxWidth="bg"  className={containerStyle.container}>
+          <Typography component="div" className={containerStyle.typography}/>
         </Container>
 
     )
