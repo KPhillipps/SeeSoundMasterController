13c5f07e4f08ebaafc572056bb444615c77110a2
Diego Avila <dandida95@gmail.com>
2020-10-16 06:07:44 -0600

    added upload button to upload view

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {Card, makeStyles, CardContent, Typography, CardActionArea, IconButton} from '@material-ui/core';
-import {Image} from '@material-ui/icons';
+import React, {useState} from 'react';
+import {Card, makeStyles, CardContent, Typography} from '@material-ui/core';
+import {Image, } from '@material-ui/icons';
 
 
 const styles = makeStyles({
@@ -11,34 +11,51 @@ const styles = makeStyles({
          backgroundColor: '#1d1d1d',
          color: '#fff'
     }, input: {
-        display: 'none',
-        fontSize: 'large'
+        opacity: 0,
+        position: 'absolute',
+        height: 100,
+        width: 100
+
+      
     },
     image: {
-        color: '#7C0303'
+        color: '#7C0303',
+        height: 100,
+        width: 100
+    },
+    image2: {
+        position: 'relative bottom'
     }
 })
+
+
+function FileUpload() {
+
+    const inputStyle = styles()
+
+    const [file, setFile] = useState("")
+    function handleUpload(event) {
+        setFile(event.target.files[0]);
+    }
+
+    return (<div>
+        <input type="file" onChange={handleUpload} className={inputStyle.input}/>
+            <Image className={inputStyle.image}/>
+            {file && <img className={inputStyle.image2} src={URL.createObjectURL(file)}/>}
+    </div>)
+}
+
 
 function AppCard(){
 
     const cardStyle = styles()
-    let pictures = [];
-
-    const onFileChange = (event) => {
-        React.useState(pictures.push({selectedFile: event.target.files[0]}))
-    }
 
 
     return (<Card className={cardStyle.card}>
         <CardContent>
             <Typography variant="h3">Upload</Typography>
             <Typography variant="body1">Select the images to be uploaded</Typography>
-            <CardActionArea>
-                <input className={cardStyle.input} id="icon-button-file" type="file" onChange={onFileChange}/>
-                <label htmlFor="icon-button-file">
-                    <Image className={cardStyle.image} fontSize='large'/>
-                </label>
-            </CardActionArea>
+            <FileUpload/>
         </CardContent>
     </Card>)
 }
